dc8731eaafa980ecd60cc8882bf080c95db5d60d
jeremyholbrecht <Griffley@proton.me>
2024-03-04 12:00:34 +0100

    first commit

--- main.go
+++ main.go
@@ -4,15 +4,16 @@ import "fmt"
 
 type character struct {
 	name        string
+	age         int
 	devilFruit  string
 	affiliation string
 }
 
 func main() {
 	luffy := character{name: "Monkey D. Luffy", devilFruit: "Gomu Gomu no Mi", affiliation: "Straw Hat Pirates"}
-	zoro := character{name: "Roronoa Zoro", affiliation: "Straw Hat Pirates"}
+	zoro := character{name: "Roronoa Zoro", age: 17, affiliation: "Straw Hat Pirates"}
 
 	fmt.Printf("Name: %s Devil fruit: %s Affiliation: %s \n", luffy.name, luffy.devilFruit, luffy.affiliation)
-	fmt.Printf("Name: %s Affiliation: %s", zoro.name, zoro.affiliation)
+	fmt.Printf("Name: %s Age: %d Affiliation: %s", zoro.name, zoro.age, zoro.affiliation)
 
 }
